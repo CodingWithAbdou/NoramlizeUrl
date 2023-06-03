2f512eb579a483feb927567a5a635d8c4dade318
CodingWithAbdou <coder.abdou@gmail.com>
2023-06-03 15:00:24 +0100

    Add Some Tests and add function get the url from dom

--- __test__/main.test.js
+++ __test__/main.test.js
@@ -1,5 +1,5 @@
-const { checkUrl } = require("../main.js");
-const { test, expect } = require("@jest/globals");
+const { checkUrl, getLinksFromHtml } = require("../main.js");
+const { expect } = require("@jest/globals");
 
 test("test url without protocol (https and http ..)", () => {
   const input = "https://blog.abdou.com/story";
@@ -17,10 +17,79 @@ test("check the letter if uppercase or lowercase", () => {
   expect(output).toEqual(expected);
 });
 
-test("remove backslash  if was in last site", () => {
+test("remove backslash  if was it in last site with https protocol", () => {
   const input = "https://Blog.abdou.com/";
   const output = checkUrl(input);
   const expected = "blog.abdou.com";
+
+  expect(output).toEqual(expected);
+});
+
+test("remove backslash  if was it in last site with http protocol", () => {
+  const input = "http://Blog.abdou.com/";
+  const output = checkUrl(input);
+  const expected = "blog.abdou.com";
+
+  expect(output).toEqual(expected);
+});
+
+test("getLinksFromHtml Full Link", () => {
+  const inpoutHTML = `
+  <html>
+    <body>
+        <a href="http://blog.abdou.com">blog.abdou.com</a>
+    </body>
+  </html>`;
+  const output = getLinksFromHtml(inpoutHTML);
+  const expected = ["http://blog.abdou.com"];
+
+  expect(output).toEqual(expected);
+});
+
+test("getLinksFromHtml Just Path", () => {
+  const inpoutHTML = `
+  <html>
+    <body>
+        <a href="/resume">blog.abdou.com</a>
+    </body>
+  </html>`;
+  const baseUrl = "https://blog.abdou.com";
+  const output = getLinksFromHtml(inpoutHTML, baseUrl);
+  const expected = ["https://blog.abdou.com/resume"];
+
+  expect(output).toEqual(expected);
+});
+
+test("getLinksFromHtml many of links", () => {
+  const inpoutHTML = `
+  <html>
+    <body>
+        <a href="/resume">blog.abdou.com</a>
+        <a href="https://blog.abdou2.com/resume">blog.abdou2.com</a>
+        <a href="/resume">blog.abdou.com</a>
+    </body>
+  </html>`;
+  const baseUrl = "https://blog.abdou.com";
+  const output = getLinksFromHtml(inpoutHTML, baseUrl);
+  const expected = [
+    "https://blog.abdou.com/resume",
+    "https://blog.abdou2.com/resume",
+    "https://blog.abdou.com/resume",
+  ];
+
+  expect(output).toEqual(expected);
+});
+
+test("getLinksFromHtml pass Invalid url", () => {
+  const inpoutHTML = `
+  <html>
+    <body>
+        <a href="invalid">invalid</a>
+    </body>
+  </html>`;
+  const baseUrl = "https://blog.abdou.com";
+  const output = getLinksFromHtml(inpoutHTML, baseUrl);
+  const expected = [];
 
   expect(output).toEqual(expected);
 });
--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+function getLinksFromHtml(HTMLDom, baseUrl) {
+  const array = [];
+  const dom = new JSDOM(HTMLDom);
+  const links = dom.window.document.querySelectorAll("a");
+
+  for (let i = 0; i < links.length; i++) {
+    let link = links[i].href;
+
+    if (link[0] === "/") {
+      link = `${baseUrl}${link}`;
+    }
+    try {
+      let newLink = new URL(link).href;
+
+      if (newLink.slice(-1) === "/") {
+        newLink = newLink.slice(0, -1);
+      }
+
+      array.push(newLink);
+    } catch (err) {
+      console.log("You Url Is NoT Valid :", err.message);
+    }
+  }
+  return array;
+}
+
 const url = "https://Blog.abdou.com/";
 function checkUrl(url) {
   const newUrl = new URL(url);
@@ -12,4 +41,5 @@ function checkUrl(url) {
 
 module.exports = {
   checkUrl,
+  getLinksFromHtml,
 };
